5ea6f4a0a72219f98a2b996c5c3abdbbb76f03fc
Mario Voigt <opensource@leyghis.de>
2020-08-02 18:27:37 +0200

    Upgrade for InkScape 1.0

--- render_fibonaccipattern.py
+++ render_fibonaccipattern.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python 
+#!/usr/bin/env python3
 '''
 Copyright (C) 2015-2015 Carlos Mostek carlosmostek@gmail.com
 
@@ -16,23 +16,19 @@ You should have received a copy of the GNU General Public License
 along with this program; if not, write to the Free Software
 Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 '''
-# standard library
 import math
-from math import pi
-
-# local library
 import inkex
-import simplestyle
+from lxml import etree
 
-inkex.localize()
+inkex.localization.localize
 
 # This is basically the draw method from the help guides for inkscape
-def draw_SVG_ellipse(rx, ry, cx, cy, parent, start_end=(0,2*pi),transform='' ):
+def draw_SVG_ellipse(rx, ry, cx, cy, parent, start_end=(0,2*math.pi),transform='' ):
 
     style = {   'stroke'        : '#000000',
                 'stroke-width'  : '1',
                 'fill'          : 'none'            }
-    ell_attribs = {'style':simplestyle.formatStyle(style),
+    ell_attribs = {'style':str(inkex.Style(style)),
         inkex.addNS('cx','sodipodi')        :str(cx),
         inkex.addNS('cy','sodipodi')        :str(cy),
         inkex.addNS('rx','sodipodi')        :str(rx),
@@ -43,7 +39,7 @@ def draw_SVG_ellipse(rx, ry, cx, cy, parent, start_end=(0,2*pi),transform='' ):
         inkex.addNS('type','sodipodi')      :'arc',
         'transform'                         :transform
             }
-    ell = inkex.etree.SubElement(parent, inkex.addNS('path','svg'), ell_attribs )
+    ell = etree.SubElement(parent, inkex.addNS('path','svg'), ell_attribs )
 
 # This is the workhorse, it draws the circle based on which node number
 def drawKthCircle(k,firstRadius,lastRadius,numNodes,spreadFactor,parent):
@@ -56,7 +52,7 @@ def drawKthCircle(k,firstRadius,lastRadius,numNodes,spreadFactor,parent):
 	
 	# Calculate X and Y from theta = 2 pi phi k and radius = sqrt(k)
 	r = spreadFactor * math.sqrt(k)
-	theta = 2*pi*phi*k
+	theta = 2*math.pi*phi*k
 
 	# use simple trig to get cx and cy
 	x = r * math.cos(theta)
@@ -72,35 +68,22 @@ def drawKthCircle(k,firstRadius,lastRadius,numNodes,spreadFactor,parent):
 class FibonacciSpiral(inkex.Effect):
 	def __init__(self):
 		inkex.Effect.__init__(self)
-		self.OptionParser.add_option("-f", "--FirstRadius",
-						action="store", type="int", 
-						dest="firstRadius", default="5",
-						help="The radius of the first layer of circles in pixels.")
-		self.OptionParser.add_option("-l", "--LastRadius",
-						action="store", type="int", 
-						dest="lastRadius", default="10",
-						help="The radius of the last layer of circles in pixels.")
-		self.OptionParser.add_option("-n", "--NumberOfNodes",
-						action="store", type="int", 
-						dest="numNodes", default="5",
-						help="The number of layers in the fibonacci spiral")
-		self.OptionParser.add_option("-s", "--SpreadFactor",
-						action="store", type="int", 
-						dest="spreadFactor", default="10",
-						help="This will create a larger spread between the nodes from the center.")
+		self.arg_parser.add_argument("-f", "--FirstRadius",	type=int, default="5", help="The radius of the first layer of circles in pixels.")
+		self.arg_parser.add_argument("-l", "--LastRadius",type=int, default="10", help="The radius of the last layer of circles in pixels.")
+		self.arg_parser.add_argument("-n", "--NumberOfNodes", type=int, default="5", help="The number of layers in the fibonacci spiral")
+		self.arg_parser.add_argument("-s", "--SpreadFactor",type=int, default="10", help="This will create a larger spread between the nodes from the center.")
 
 	def effect(self):
 		# Foreach Node
-		for k in range(1,self.options.numNodes):
+		for k in range(1,self.options.NumberOfNodes):
 			# Draw the circle
 			drawKthCircle(k,
-				self.options.firstRadius,
-				self.options.lastRadius,
-				self.options.numNodes,
-				self.options.spreadFactor,
-				self.current_layer)
+				self.options.FirstRadius,
+				self.options.LastRadius,
+				self.options.NumberOfNodes,
+				self.options.SpreadFactor,
+				self.svg.get_current_layer())
 			
 if __name__ == '__main__':
     e = FibonacciSpiral()
-    e.affect()
-
+    e.run()
